0fedd2ff9af33d9c1dcc0dcd85171ca7ac338555
Pawel <ssopel10@gmail.com>
2020-04-09 18:30:38 +0200

    Updates

---
+++ precache-manifest.99f20851b47192e8fd60c178d5f3c623.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6472c0c3f3cca0ab8d3bddce4bc395a4",
+    "url": "/utp-project/index.html"
+  },
+  {
+    "revision": "605b9b1d0bfa5da50049",
+    "url": "/utp-project/static/css/main.d9b33b2d.chunk.css"
+  },
+  {
+    "revision": "81a0f96d26f16968a2a1",
+    "url": "/utp-project/static/js/2.33bbe7e0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/utp-project/static/js/2.33bbe7e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "605b9b1d0bfa5da50049",
+    "url": "/utp-project/static/js/main.d2923127.chunk.js"
+  },
+  {
+    "revision": "b57acc1dd7d1be394867",
+    "url": "/utp-project/static/js/runtime-main.6b9caf67.js"
+  }
+]);
--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import SignUp from './components/SignUp';
-
-function App() {
-  return <SignUp />;
-}
-
-export default App;
